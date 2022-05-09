79560c069e06c099ba42ed13f8a42052080862d8
BrolyTheVVF <BrolyTheVVF@gmail.com>
2022-05-09 22:19:28 +0200

    Create WAC.loc.js

---
+++ Translation/Server/WAC.loc.js
@@ -0,0 +1,42 @@
+var locale = {};
+
+locale["WAC.notice.ban.temporary.indefinite"] = {
+	"en": "This character has been banned for un indefinite period of time. Ban reason: {0}",
+	"fr": "Ce personnage a été banis pour une durée indeterminée. Raison du ban: {0}",
+	"es": "",
+	"zh-cn": "",
+	"zh-tw":"",
+	"kr": "",
+	"ru": "",
+	"tr": "",
+	"de": "",
+	"jp": "",
+	"pt": "",
+	"vi": "",
+	"ar": "",
+	"it": "",
+	"pl": "",
+	"th": "",
+	"id": ""
+};
+locale["WAC.notice.ban.temporary.definite"] = {
+	"en": "This character is banned until {1}. Ban reason: {0}",
+	"fr": "Ce personnage est banis pendant {1}. Raison du ban: {0}",
+	"es": "",
+	"zh-cn": "",
+	"zh-tw":"",
+	"kr": "",
+	"ru": "",
+	"tr": "",
+	"de": "",
+	"jp": "",
+	"pt": "",
+	"vi": "",
+	"ar": "",
+	"it": "",
+	"pl": "",
+	"th": "",
+	"id": ""
+};
+
+exports.locale = locale;
