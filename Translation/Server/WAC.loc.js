0b6efae96fab5360d7feb41f86fbe8868ab517ca
Llayth <33810274+Lleyth@users.noreply.github.com>
2022-05-10 10:46:45 +0200

    Update WAC.loc.js

--- Translation/Server/WAC.loc.js
+++ Translation/Server/WAC.loc.js
@@ -2,7 +2,7 @@ var locale = {};
 
 locale["WAC.notice.ban.temporary.indefinite"] = {
 	"en": "This character has been banned for un indefinite period of time. Ban reason: {0}",
-	"fr": "Ce personnage a été banis pour une durée indeterminée. Raison du ban: {0}",
+	"fr": "Ce personnage a été banni pour une durée indéterminée. Raison du ban: {0}",
 	"es": "",
 	"zh-cn": "",
 	"zh-tw":"",
@@ -21,7 +21,7 @@ locale["WAC.notice.ban.temporary.indefinite"] = {
 };
 locale["WAC.notice.ban.temporary.definite"] = {
 	"en": "This character is banned until {1}. Ban reason: {0}",
-	"fr": "Ce personnage est banis pendant {1}. Raison du ban: {0}",
+	"fr": "Ce personnage est banni pendant {1}. Raison du ban: {0}",
 	"es": "",
 	"zh-cn": "",
 	"zh-tw":"",
@@ -40,7 +40,7 @@ locale["WAC.notice.ban.temporary.definite"] = {
 };
 locale["WAC.notice.ban.auto-detect"] = {
 	"en": "The WAC system detected a suspicious activity and kicked you from the game.",
-	"fr": "Le système WAC a detecté une activité suspecte et vous à ejecté du jeu.",
+	"fr": "Le système WAC a detecté une activité suspecte et vous a éjecté du jeu.",
 	"es": "",
 	"zh-cn": "",
 	"zh-tw":"",
@@ -59,7 +59,7 @@ locale["WAC.notice.ban.auto-detect"] = {
 };
 locale["WAC.notice.ban.auto-detect.warning"] = {
 	"en": "The WAC system detected a suspicious activity and kicked you from the game, and a WAC warning has also been added to your account. This is definitive and cannot be revoked.",
-	"fr": "Le système WAC a detecté une activité suspecte et vous à ejecté du jeu, et un avertissement WAV a été ajouté a votre compte. Cet avertissement est definitif et ne peut pas etre effacé.",
+	"fr": "Le système WAC a detecté une activité suspecte et vous a ejecté du jeu, et un avertissement WAV a été ajouté à votre compte. Cet avertissement est definitif et ne peut pas être effacé.",
 	"es": "",
 	"zh-cn": "",
 	"zh-tw":"",
