2aa32d1d8980d2d3617bc037aa4902bd2aefd9ad
BrolyTheVVF <BrolyTheVVF@gmail.com>
2023-11-28 19:01:18 +0100

    Quest file rework

---
+++ Translation/Server/quest.other.loc.js
@@ -0,0 +1,23 @@
+var locale = {};
+
+locale["quest.objective.reachLevel"] = {
+	"en": "Upgrade to Lv.{0}",
+	"fr": "Passez Niveau {0}",
+	"es": "",
+	"zh-cn": "升到{0}等级",
+	"zh-tw":"",
+	"kr": "",
+	"ru": "",
+	"tr": "",
+	"de": "",
+	"jp": "",
+	"pt": "",
+	"vi": "Hãy đạt cấp {0}",
+	"ar": "",
+	"it": "",
+	"pl": "",
+	"th": "",
+	"id": ""
+};
+
+exports.locale = locale;
